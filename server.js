0c99fbd5abde82682f080640bee7b13db32e0d5d
pmayur <thisispatil@gmail.com>
2020-12-05 17:28:10 +0530

    Add endpoint to upload file

--- server.js
+++ server.js
@@ -5,14 +5,28 @@ require('dotenv').config()
 var app = express();
 
 app.use(cors());
+
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
 
+  let fileName = req.file.originalname;
+  let fileType = req.file.mimetype;
+  let fileSize = req.file.size;
 
+  res.json({
+    "name": fileName,
+    "type": fileType,
+    "size": fileSize
+  })
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
