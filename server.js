3cbb6785932ea68357e215b61b5f68bbdc03c8a2
pmayur <thisispatil@gmail.com>
2020-12-05 17:31:13 +0530

    Add functionality to clear folder after upload

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ var app = express();
 
 app.use(cors());
 
+// multer config
 var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' });
+
+// filesystem config
+const fs = require('fs');
+const path = require('path');
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -15,11 +20,23 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// api for uploading file
 app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
 
   let fileName = req.file.originalname;
   let fileType = req.file.mimetype;
   let fileSize = req.file.size;
+
+  // clear folder as file unrequired
+  fs.readdir('uploads', (err, files) => {
+    if (err) throw err;
+  
+    for (const file of files) {
+      fs.unlink(path.join('uploads', file), err => {
+        if (err) throw err;
+      });
+    }
+  });
 
   res.json({
     "name": fileName,
